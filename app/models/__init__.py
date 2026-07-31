d51139b01b0744c6527a3cf1213579f6a89ed400
bonyvah <bekbolsunysmanov07@gmail.com>
2026-07-31 23:32:50 +0600

    fix:remove error ignoring comments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from app.models.user import User # type: ignore
-from app.models.project import Project # type: ignore
-from app.models.membership import Membership # type: ignore
-from app.models.document import Document # type: ignore
+from app.models.user import User
+from app.models.project import Project
+from app.models.membership import Membership
+from app.models.document import Document
