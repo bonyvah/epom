a16d0620a46de03922b942e96b2a5579b707d2b2
bonyvah <bekbolsunysmanov07@gmail.com>
2026-08-05 13:28:34 +0600

    fix: convert sub into uuid in get current user dep

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -15,7 +16,7 @@ async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: AsyncSession = Depends(get_db),
 ) -> User:
-    user_id = decode_access_token(token)
+    user_id = uuid.UUID(decode_access_token(token))
     result = await db.execute(
         select(User).where(User.id == user_id, User.deleted_at.is_(None))
     )
