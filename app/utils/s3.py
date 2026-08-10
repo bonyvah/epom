be7d797366388dfbe2fb3ff6d63257add6e95f18
bonyvah <bekbolsunysmanov07@gmail.com>
2026-08-10 21:10:09 +0600

    feat: fix s3 util funcs

--- app/utils/s3.py
+++ app/utils/s3.py
@@ -1,16 +1,24 @@
 import boto3
 from app.config import settings
 
-s3 = boto3.client('s3')
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=settings.aws_access_key_id,
+    aws_secret_access_key=settings.aws_secret_access_key,
+    region_name=settings.aws_region,
+)
 
-def upload_file(key:str, file) -> dict:
-    return s3.put_object(Bucket=settings.s3_bucket_name, Key=key, Body=file)
+def upload_file(key: str, contents: bytes, content_type: str | None) -> None:
+    s3.put_object(
+        Bucket=settings.s3_bucket_name, Key=key, Body=contents, ContentType=content_type
+    )
 
-def fetch_file(key:str):
-    with open ("filename","wb") as data:
-        s3.download_fileobj(settings.s3_bucket_name, key, data)
-    print(data)
-    return data
+def generate_presigned_url(key: str, expires_in: int = 3600) -> str:
+    return s3.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": settings.s3_bucket_name, "Key": key},
+        ExpiresIn=expires_in,
+    )
 
-def delete_file(key:str) -> bool:
-    return s3.delete_object(Bucket=settings.s3_bucket_name, Key=key)['DeleteMarker']
+def delete_file(key: str):
+    s3.delete_object(Bucket=settings.s3_bucket_name, Key=key)
