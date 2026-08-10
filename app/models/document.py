fba4735041c17f105eb57307ea5134723b2350cf
bonyvah <bekbolsunysmanov07@gmail.com>
2026-08-10 21:09:19 +0600

    fix: rm deleted_at field

--- app/models/document.py
+++ app/models/document.py
@@ -27,6 +27,3 @@ class Document(Base):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), server_onupdate=func.now()
     )
-    deleted_at: Mapped[datetime | None] = mapped_column(
-        DateTime(timezone=True), nullable=True
-    )
