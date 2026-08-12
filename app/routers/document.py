a023a77c19d1bae2f1af929497a3bc8964316559
bonyvah <bekbolsunysmanov07@gmail.com>
2026-08-13 00:10:46 +0600

    feat(document): implement single-document upload and storage verification

--- app/routers/document.py
+++ app/routers/document.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, UploadFile, File, status
 from typing import Annotated, List
 from uuid import UUID
 
-from app.schemas.document import DocumentUpdate, DocumentResponse
+from app.schemas.document import DocumentUpdate, DocumentResponse #, UploadDocumentsResponse
 from app.services import document as document_service
 from app.dependencies import CurrentUser
 from app.database import DBSession
@@ -15,42 +15,45 @@ router = APIRouter(tags=["document"])
 async def get_project_documents(id: UUID, current_user: CurrentUser, db: DBSession):
     return await document_service.get_project_documents(id, current_user, db)
 
+@router.post("/project/{id}/document", response_model=DocumentResponse)
+async def upload_document_to_project(id: UUID, file: UploadFile, current_user: CurrentUser, db:DBSession):
+    return await document_service.upload_document_to_project(id, file, current_user,db)
 
-@router.post(
-    "/project/{id}/documents",
-    response_model=list[DocumentResponse],
-    openapi_extra={
-        "requestBody": {
-            "content": {
-                "multipart/form-data": {
-                    "schema": {
-                        "type": "object",
-                        "required": ["files"],
-                        "properties": {
-                            "files": {
-                                "type": "array",
-                                "items": {"type": "string", "format": "binary"},
-                                "description": "Files to upload",
-                            }
-                        },
-                    }
-                }
-            },
-            "required": True,
-        }
-    },
-)
-async def upload_project_documents(id: UUID, current_user: CurrentUser, db: DBSession, files: Annotated[List[UploadFile], File(description="Files to upload")]):
-    return await document_service.upload_documents_to_project(id, files, current_user, db)
-
-@router.put("/documents/{id}", response_model=DocumentResponse)
+@router.put("/document/{id}", response_model=DocumentResponse)
 async def update_document(id: UUID, body: DocumentUpdate, current_user: CurrentUser, db: DBSession):
     return await document_service.update_document(id, body, current_user, db)
 
-@router.get("/documents/{id}", response_model=dict)
+@router.get("/document/{id}", response_model=dict)
 async def download_file(id: UUID, current_user: CurrentUser, db: DBSession):
     return await document_service.download_file(id, current_user, db)
 
 @router.delete("/document/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_document(id: UUID, current_user: CurrentUser, db: DBSession):
     await document_service.delete_document(id, current_user, db)
+
+# @router.post(
+#     "/project/{id}/documents",
+#     response_model=UploadDocumentsResponse,
+#     openapi_extra={
+#         "requestBody": {
+#             "content": {
+#                 "multipart/form-data": {
+#                     "schema": {
+#                         "type": "object",
+#                         "required": ["files"],
+#                         "properties": {
+#                             "files": {
+#                                 "type": "array",
+#                                 "items": {"type": "string", "format": "binary"},
+#                                 "description": "Files to upload",
+#                             }
+#                         },
+#                     }
+#                 }
+#             },
+#             "required": True,
+#         }
+#     },
+# )
+# async def upload_project_documents(id: UUID, current_user: CurrentUser, db: DBSession, files: Annotated[List[UploadFile], File(description="Files to upload")]):
+#     return await document_service.upload_documents_to_project(id, files, current_user, db)
--- app/services/document.py
+++ app/services/document.py
@@ -1,29 +1,33 @@
+import filetype
 from fastapi import HTTPException, status, UploadFile
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from uuid import UUID, uuid4
+import logging
 
 from app.models import User, Document, Membership, Project
 from app.database import AsyncSession
 from app.services.project import _get_current_user_project
 from app.schemas.document import DocumentUpdate
-from app.utils.s3 import upload_file,  delete_file, generate_presigned_url
+from app.utils.s3 import upload_file, delete_file, generate_presigned_url
+from app.utils.file import validate_and_infer_mime
 from app.config import settings
 
 async def _get_current_user_document(id: UUID, current_user: User, db: AsyncSession):
     result = await db.execute(
-            select(Document)
-            .join(Project, Document.project_id == Project.id)
-            .join(Membership, Project.id == Membership.project_id)
-            .where(
-                Document.id == id,
-                Membership.user_id == current_user.id,
-                Project.deleted_at.is_(None)
-            )
+        select(Document)
+        .join(Project, Document.project_id == Project.id)
+        .join(Membership, Project.id == Membership.project_id)
+        .where(
+            Document.id == id,
+            Membership.user_id == current_user.id,
+            Project.deleted_at.is_(None),
         )
-    
+    )
+
     return result.scalar_one_or_none()
+
 
 async def get_project_documents(
     project_id: UUID, current_user: User, db: AsyncSession
@@ -35,68 +39,84 @@ async def get_project_documents(
             status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
         )
 
-    result = await db.execute(
-        select(Document).where(
-            Document.project_id == project_id
-        )
-    )
+    result = await db.execute(select(Document).where(Document.project_id == project_id))
 
     return list(result.scalars().all())
 
-async def upload_documents_to_project(
-    project_id: UUID,
-    files: list[UploadFile],
-    current_user: User,
-    db: AsyncSession,
-) -> list[Document]:
-    project = await _get_current_user_project(project_id, current_user, db)
 
+async def upload_document_to_project(
+    project_id: UUID, file: UploadFile, current_user: User, db: AsyncSession
+) -> Document:
+    # 1. access check
+    project = await _get_current_user_project(project_id, current_user, db)
     if not project:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
         )
 
-    docs = []
-    uploaded_keys = []
-    for file in files:
-        contents = await file.read()
+    # Read the file contents
+    contents = await file.read()
+    await file.seek(0)
+    file_size = len(contents)
 
-        if len(contents) > settings.file_size_limit_mb * 1024 * 1024:
-            raise HTTPException(status_code=413, detail=f"{file.filename} exceeds {settings.file_size_limit_mb}MB limit")
-
-        document_id = uuid4()
-        key = f"{document_id}/{file.filename}"
-        content_type = file.content_type or "application/octet-stream"
-
-        try:
-            await upload_file(key, contents, content_type)
-            uploaded_keys.append(key)
-        except Exception:
-            for key in uploaded_keys:
-                await delete_file(key)
-            raise HTTPException(
-                status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                detail=f"Failed to upload {file.filename}"
-            )
-
-        document = Document(
-            id=document_id,
-            project_id=project_id,
-            name=file.filename,
-            s3_key=key,
-            size_bytes=len(contents),
-            content_type=content_type,
-            uploaded_by=current_user.id,
+    # 2. Magic Bytes & Type Validation (Using the utility)
+    try:
+        inferred_content_type = validate_and_infer_mime(file.filename or "", contents)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
         )
-        docs.append(document)
-        db.add(document)
 
+    
+    # 3. size checks
+    if file_size > project.document_size_limit_mb * 1024 * 1024:
+        raise HTTPException(
+            status_code=status.HTTP_413_CONTENT_TOO_LARGE,
+            detail=f"File exceeds project document limit of {project.document_size_limit_mb}MB",
+        )
+
+    # check project limit
+    total_size_query = await db.execute(
+        select(func.sum(Document.size_bytes)).where(Document.project_id == project_id)
+    )
+    current_total = total_size_query.scalar() or 0
+
+    if current_total + file_size > project.project_size_limit_gb * 1024 * 1024 * 1024:
+        raise HTTPException(
+            status_code=status.HTTP_413_CONTENT_TOO_LARGE,
+            detail=f"Upload would exceed project total size limit of {project.project_size_limit_gb}GB",
+        )
+
+    # 4. s3 upload
+    document_id = uuid4()
+    key = f"{document_id}/{file.filename}"
+    content_type = file.content_type or "application/octet-stream"
+
+    try:
+        await upload_file(key, contents, content_type)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Failed to upload file",
+        )
+
+    # 5. save to db
+    document = Document(
+        id=document_id,
+        project_id=project_id,
+        name=file.filename,
+        s3_key=key,
+        size_bytes=file_size,
+        content_type=content_type,
+        uploaded_by=current_user.id,
+    )
+    db.add(document)
     await db.commit()
+    await db.refresh(document)
 
-    for d in docs:
-        await db.refresh(d)
+    return document
 
-    return docs
 
 async def download_file(id: UUID, current_user: User, db: AsyncSession) -> dict:
     result = await db.execute(
@@ -119,7 +139,10 @@ async def download_file(id: UUID, current_user: User, db: AsyncSession) -> dict:
     url = await generate_presigned_url(document.s3_key)
     return {"download_url": url}
 
-async def update_document(id: UUID,  body: DocumentUpdate, current_user: User, db: AsyncSession) -> Document:
+
+async def update_document(
+    id: UUID, body: DocumentUpdate, current_user: User, db: AsyncSession
+) -> Document:
     document = await _get_current_user_document(id, current_user, db)
 
     if not document:
@@ -132,6 +155,7 @@ async def update_document(id: UUID,  body: DocumentUpdate, current_user: User, d
     await db.commit()
     await db.refresh(document)
     return document
+
 
 async def delete_document(id: UUID, current_user: User, db: AsyncSession):
 
@@ -147,7 +171,75 @@ async def delete_document(id: UUID, current_user: User, db: AsyncSession):
     except Exception:
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail="Failed to delete file"
+            detail="Failed to delete file",
         )
     await db.delete(document)
     await db.commit()
+
+
+# async def upload_documents_to_project(
+#     project_id: UUID,
+#     files: list[UploadFile],
+#     current_user: User,
+#     db: AsyncSession,
+# ) -> dict:
+#     #1)
+#     project = await _get_current_user_project(project_id, current_user, db)
+
+#     if not project:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+#         )
+
+#     doc_limit_bytes = project.document_size_limit_mb * 1024 * 1024
+#     project_limit_bytes = project.project_size_limit_gb * 1024 * 1024 * 1024
+
+#     total_size_query = await db.execute(
+#         select(func.sum(Document.size_bytes)).where(Document.project_id == project_id)
+#     )
+#     current_total_size = total_size_query.scalar() or 0
+
+#     docs = []
+#     uploaded_keys = []
+#     for file in files:
+#         contents = await file.read()
+
+#         if len(contents) > doc_limit_bytes:
+#             raise HTTPException(
+#                 status_code=status.HTTP_413_CONTENT_TOO_LARGE,
+#                 detail=f"File {file.filename} exceeds project document limit of {project.document_size_limit_mb}MB",
+#             )
+
+#         document_id = uuid4()
+#         key = f"{document_id}/{file.filename}"
+#         content_type = file.content_type or "application/octet-stream"
+
+#         try:
+#             await upload_file(key, contents, content_type)
+#             uploaded_keys.append(key)
+#         except Exception:
+#             for key in uploaded_keys:
+#                 await delete_file(key)
+#             raise HTTPException(
+#                 status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+#                 detail=f"Failed to upload {file.filename}",
+#             )
+
+#         document = Document(
+#             id=document_id,
+#             project_id=project_id,
+#             name=file.filename,
+#             s3_key=key,
+#             size_bytes=len(contents),
+#             content_type=content_type,
+#             uploaded_by=current_user.id,
+#         )
+#         docs.append(document)
+#         db.add(document)
+
+#     await db.commit()
+
+#     for d in docs:
+#         await db.refresh(d)
+
+#     return docs
